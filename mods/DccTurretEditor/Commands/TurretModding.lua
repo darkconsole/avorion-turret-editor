9894a6d1507d49e7e4ad7dd3c53dff9e3625594e
darkconsole <darkcee.legit@gmail.com>
2018-02-24 01:05:01 -0600

    add a moron alarm

--- mods/DccTurretEditor/Commands/TurretModding.lua
+++ mods/DccTurretEditor/Commands/TurretModding.lua
@@ -16,6 +16,23 @@ function initialize(Command,...)
 
 	--------
 
+	local ConfigOK, Config = pcall(
+		require,
+		'mods.DccTurretEditor.Config'
+	)
+
+	if(not ConfigOK)
+	then
+		deferredCallback(
+			1, "WeDoneHereAndThere",
+			"Weapon Engineering: Config.lua Error",
+			"Did you remember to copy ConfigDefault.lua?"
+		)
+		return
+	end
+
+	--------
+
 	if(onServer())
 	then
 		print("[DccTurretEditor] Adding Turret Modding to " .. PlayerRef.name .. " on " .. Ship.name)
@@ -23,4 +40,17 @@ function initialize(Command,...)
 	end
 
 	return terminate()
+end
+
+function WeDoneHereAndThere(Title,Text)
+	if(onServer())
+	then
+		invokeClientFunction(Player(),"WeDoneHereAndThere",Title,Text)
+		terminate()
+		return
+	end
+
+	displayMissionAccomplishedText(Title,Text)
+	terminate()
+	return
 end
