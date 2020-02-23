ce54583210a827aaba7f34979714d7651c8d3a0a
darkconsole <darkcee.legit@gmail.com>
2020-02-23 03:53:13 -0600

    update my tools

---
+++ tools/push.bat
@@ -0,0 +1,5 @@
+
+REM cp -rv data mods modinfo.lua %APPDATA%\Avorion\mods\DccWeaponEngineering
+xcopy /E /I data %APPDATA%\Avorion\mods\DccWeaponEngineering\data
+xcopy /E /I mods %APPDATA%\Avorion\mods\DccWeaponEngineering\mods
+xcopy modinfo.lua %APPDATA%\Avorion\mods\DccWeaponEngineering
