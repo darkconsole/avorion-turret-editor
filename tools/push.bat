4504c362f1d635ab2474148e29e24544d26b54f8
darkconsole <darkcee.legit@gmail.com>
2020-02-28 03:03:08 -0600

    force overwrite

--- tools/push.bat
+++ tools/push.bat
@@ -1,5 +1,5 @@
 
 REM cp -rv data mods modinfo.lua %APPDATA%\Avorion\mods\DccWeaponEngineering
-xcopy /E /I data %APPDATA%\Avorion\mods\DccWeaponEngineering\data
-xcopy /E /I mods %APPDATA%\Avorion\mods\DccWeaponEngineering\mods
-xcopy modinfo.lua %APPDATA%\Avorion\mods\DccWeaponEngineering
+xcopy /E /I/Y  data %APPDATA%\Avorion\mods\DccWeaponEngineering\data
+xcopy /E /I/Y  mods %APPDATA%\Avorion\mods\DccWeaponEngineering\mods
+xcopy /Y modinfo.lua %APPDATA%\Avorion\mods\DccWeaponEngineering
